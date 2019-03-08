383577850c6047d4466325bc061da2dec70c20b2
kukjevov <kukjevov@gmail.com>
2019-03-08 10:50:14 +0100

    INT: added forgotten export of code options gatherer

--- src/misc/index.ts
+++ src/misc/index.ts
@@ -2,5 +2,6 @@ export * from './ngSelectOptions.interface';
 export * from './optionsGatherer/optionsGatherer.interface';
 export * from './optionsGatherer/dynamicOptionsGatherer';
 export * from './optionsGatherer/dynamicOptionsGatherer.interface';
+export * from './optionsGatherer/codeOptionsGatherer';
 export * from './plugin.interface';
 export * from './templateGatherer.interface';
